5babf8c5f0913b57d5baa8ec3d0b9a4ae10eaa6f
Sean <hedsean@gmail.com>
2020-01-03 04:31:57 +0200

    fixed updateAmount

--- src/main/java/com/seanhed/services/ItemServiceImpl.java
+++ src/main/java/com/seanhed/services/ItemServiceImpl.java
@@ -2,7 +2,6 @@ package com.seanhed.services;
 
 import java.util.List;
 import java.util.Optional;
-
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +12,7 @@ import org.springframework.stereotype.Service;
 import com.seanhed.data.beans.Item;
 import com.seanhed.data.repo.ItemRepository;
 import com.seanhed.services.dao.ItemService;
+import com.seanhed.utils.Log;
 import com.seanhed.utils.ResponseUtil;
 
 @Service
@@ -37,7 +37,7 @@ public class ItemServiceImpl implements ItemService {
 	public ResponseEntity<Object> getAllItems() {
 		try {
 			List<Item> items = itemRepository.findAll();
-			System.out.println("items retrived are -> " + items);
+			Log.info("items retrived are -> " + items);
 			return new ResponseEntity<>(items, HttpStatus.ACCEPTED);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -48,7 +48,7 @@ public class ItemServiceImpl implements ItemService {
 	@Override
 	public ResponseEntity<Object> addNewItem(Item item) {
 		try {
-			System.out.println("added item is " + item);
+			Log.info("added item is " + item);
 			return ResponseUtil.generateSuccessMessage(itemRepository.save(item).toString());
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -59,20 +59,10 @@ public class ItemServiceImpl implements ItemService {
 	@Override
 	public ResponseEntity<Object> updateItem(Item item) {
 		try {
-			System.out.println("updateItem item  " + item);
+			Log.info("updateItem item  " + item);
 			Item tempItem = itemRepository.getOne(item.getItemNo());
 			tempItem.setName(item.getName());
 			tempItem.setInventoryCode(item.getInventoryCode());
-
-			// updating amount
-			System.out.println("old amount - " + tempItem.getAmount() + ", new amount - " + item.getAmount());
-			if ( item.getAmount() < 0) {
-				return ResponseUtil.generateErrorCode(400, "cannot change to negative quantity");
-			} else if (tempItem.getAmount() > item.getAmount()) {
-				withdrawItemAmount(item.getItemNo(), tempItem.getAmount() - item.getAmount());
-			} else if (tempItem.getAmount() < item.getAmount()) {
-				depositItemAmount(item.getItemNo(), item.getAmount() - tempItem.getAmount());
-			}
 			itemRepository.save(tempItem);
 			return ResponseUtil.generateSuccessMessage(itemRepository.save(item).toString());
 		} catch (Exception e) {
@@ -80,6 +70,8 @@ public class ItemServiceImpl implements ItemService {
 			return ResponseUtil.generateErrorCode(500, "failed to update item");
 		}
 	}
+	
+
 
 	@Override
 	public ResponseEntity<Object> deleteItemFromStock(long itemNo) {
--- src/test/java/com/seanhed/exercise/ExerciseApplicationTests.java
+++ src/test/java/com/seanhed/exercise/ExerciseApplicationTests.java
@@ -1,6 +1,6 @@
 package com.seanhed.exercise;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
